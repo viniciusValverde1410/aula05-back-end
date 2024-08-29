2f4b27e526ad8b3b0641e4c93eabafa234d8187c
vinicius.valverde@aluno.senai.br <vinicius.valverde@aluno.senai.br>
2024-08-29 08:41:10 -0300

    feat: change port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 
 config()
 
-const serverPort = process.env.PORT || 3000
+const serverPort = process.env.PORT || 4000
 
 const app = express()
 app.use(express.json())
