cb6a6d199a399a237329be471da6eae23b548d6b
vinicius.valverde@aluno.senai.br <vinicius.valverde@aluno.senai.br>
2024-09-05 17:19:59 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,10 +20,12 @@ let emocoes = [
     }
 ];
 
+//Rota para buscar todas as emoções;
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes);
 });
 
+// Rota para criar uma nova emoção;
 emocoesRoutes.post("/", (req, res) => {
     const { nome, cor } = req.body;
 
@@ -35,7 +37,25 @@ emocoesRoutes.post("/", (req, res) => {
 
     emocoes.push(novaEmocao);
     
-    return res.status(201).send(emocoes);
+    return res.status(201).send(novaEmocao);
+});
+
+emocoesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+// console.log(id);
+    const emocao = emocoes.find((emotion) => emotion.id == id )
+
+    if (!emocao) {
+        return res.status(404).send({
+            message: "Emoção não encontrada!"
+        });
+    } 
+
+    return res.status(200).send({
+        message: "Emoção encontrada",
+        emocao,
+    });
 });
 
 export default emocoesRoutes
