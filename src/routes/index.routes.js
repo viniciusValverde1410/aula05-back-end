73392469fbdaa231e57ba6556d22d8bfcb10aa14
vinicius.valverde@aluno.senai.br <vinicius.valverde@aluno.senai.br>
2024-09-26 17:02:14 -0300

    feat: start to create characters routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import emocoesRoutes from "./emocoes.routes.js";
+import personagensRoutes from "./personagens.routes.js";
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.get("/2tds2", (req, res) => {
 });
 
 routes.use("/emocoes", emocoesRoutes);
+routes.use("/personagens", personagensRoutes);
 
 export default routes;
--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 
 const personagensRoutes = Router()
 
-const personagens = [
+let personagens = [
     {
         id: 100,
         nome: "Homem de Ferro",
@@ -23,6 +23,41 @@ const personagens = [
     }
 ]
 
-app.get("/personagens" , (req, res) => {
-    return res.status(200).send(personagens)
+personagensRoutes.get("/", (req, res) => {
+    return res.status (200)
+    .send(personagens)
 })
+
+personagensRoutes.post("/", (req, res) => {
+    const { nome, studio, vivo } = req.body;
+    const novoPersonagem = {
+        id: personagens.length + 1,
+        nome: nome,
+        studio: studio,
+        vivo: vivo,
+    };
+
+    personagens.push(novoPersonagem);
+    return res.status(201).send(novoPersonagem);
+});
+
+personagensRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const personagem = personagens.find((person) => person.id == id )
+
+    if (!personagem) {
+        return res.status(404).send({
+            message: "Personagem não encontrado!"
+        });
+    }
+
+    personagens = personagens.filter((person) => person.id != id)
+
+    return res.status(200).send({
+        message: "Personagem deletado",
+        personagem,
+    });
+});
+
+export default personagensRoutes
